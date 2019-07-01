17f9c20e3ee91decf9e3eb888b0fc90d3609a3c0
Ribelo <r.krzywaznia@gmail.com>
2019-07-01 20:45:39 +0200

    add doc-types and some keywords

--- src/ribelo/symplex/rotation.clj
+++ src/ribelo/symplex/rotation.clj
@@ -4,6 +4,7 @@
    [clojure.java.io :as io]
    [clojure.string :as str]
    [taoensso.encore :as e]
+   [ribelo.wombat :as wb]
    [ribelo.wombat.io :as wio]
    [net.cgrand.xforms :as x]))
 
@@ -15,7 +16,7 @@
 (defn translate-contractor [s]
   (cond
     (re-find #"(?iu)sklep 1" s) "f01451"
-    (re-find #"(?iu)sklep 2" s) "sklep2"
+    (re-find #"(?iu)sklep 2" s) "s2"
     (re-find #"(?iu)sklep 3" s) "f01450"
     (re-find #"(?iu)sklep 4" s) "f01752"
     :else                        s))
@@ -23,46 +24,54 @@
 (defn translate-doc-type [s]
   (try
     (case s
-      "Zakup"   :delivery
-      "F.Sprz." :dispatch
-      "Paragon" :dispatch
-      "WZ/OCZ." :dispatch
-      "Mfilia-" :dispatch
-      "Mfilia+" :delivery
-      "Mmag-"   :dispatch
-      "Mmag+"   :delivery
-      "F.Kor.S" :delivery
-      "PZ/OCZ." :delivery
-      "Ro.Wewn" :dispatch
-      "Zak.Kor" :dispatch
-      "KasyFis" :dispatch)
+      "Zakup"   :purchase/invoice
+      "F.Sprz." :sales/invoice
+      "Paragon" :sales/receipt
+      "WZ/OCZ." :sales/slip
+      "Mfilia-" :movements/out
+      "Mfilia+" :movements/in
+      "Mmag-"   :movements/out
+      "Mmag+"   :movements/in
+      "F.Kor.S" :sales/correction
+      "PZ/OCZ." :purchase/slip
+      "Ro.Wewn" :movements/out
+      "Zak.Kor" :purchase/correction
+      "KasyFis" :sales/receipt)
     (catch Exception e
       (throw (ex-info s {:s s})))))
 
 (defn read-file [file-path]
-  (wio/read-csv file-path
-                {:sep    ";"
-                 :header {:market/id                   0
-                          :product/name                2
-                          :product/ean                 3
-                          :rotation/contractor         6
-                          :rotation/document-id        7
-                          :rotation/date               8
-                          :rotation/purchase-net-price 9
-                          :rotation/sell-net-price     10
-                          :rotation/qty                11
-                          :rotation/purchase-net-vale  12
-                          :rotation/document-type      18}
-                 :parse  {:market/id                   translate-warehouse
-                          :product/name                str/lower-case
-                          :rotation/contractor         translate-contractor
-                          :rotation/document-id        str/lower-case
-                          :rotation/date               #(jt/local-date "yy.MM.dd" %)
-                          :rotation/purchase-net-price e/as-?float
-                          :rotation/sell-net-price     e/as-?float
-                          :rotation/qty                e/as-?float
-                          :rotation/purchase-net-vale  e/as-?float
-                          :rotation/document-type      translate-doc-type}}))
+  (->> (wio/read-csv file-path
+                 {:sep    ";"
+                  :header {:market/id                   0
+                           :product/name                2
+                           :product/ean                 3
+                           :rotation/contractor         6
+                           :rotation/document-id        7
+                           :rotation/date               8
+                           :rotation/purchase-net-price 9
+                           :rotation/sell-net-price     10
+                           :rotation/qty                11
+                           :rotation/purchase-net-value 12
+                           :rotation/document-type      18}
+                  :parse  {:market/id                   translate-warehouse
+                           :product/name                str/lower-case
+                           :rotation/contractor         translate-contractor
+                           :rotation/document-id        str/lower-case
+                           :rotation/date               #(jt/local-date "yy.MM.dd" %)
+                           :rotation/purchase-net-price e/as-?float
+                           :rotation/sell-net-price     e/as-?float
+                           :rotation/qty                e/as-?float
+                           :rotation/purchase-net-value e/as-?float
+                           :rotation/document-type      translate-doc-type}})
+       (into []
+             (comp
+              (wb/set :rotation/sell-net-value
+                      (fn [^double x ^double y]
+                        (when (and (number? x) (number? y))
+                          (* x y)))
+                      [:rotation/sell-net-price :rotation/qty])))))
+
 
 (defn read-files [{:keys [begin-date end-date data-path]}]
   (let [begin-date (cond-> begin-date (not (instance? java.time.LocalDate begin-date)) (jt/local-date))
